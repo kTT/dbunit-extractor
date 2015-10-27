50f525e02a48a2c994b5cd8ec656ddc78f5aaf12
rakk <Robert.Kapala@gmail.com>
2015-10-27 22:34:09 +0100

    Adding configuration validation

--- test/li/ktt/settings/ProjectSettingsPageTest.java
+++ test/li/ktt/settings/ProjectSettingsPageTest.java
@@ -8,6 +8,11 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -101,6 +106,30 @@ public class ProjectSettingsPageTest {
 
         // then
         verify(projectSettings).setProperties(project, false, true, true, "newVALUE");
+    }
+
+    @Test
+    public void shouldNotApplyConfiguration () {
+        // given
+        ProjectSettingsPage page = preparePageWithMocks();
+
+        // when
+        page.getExcludedColumns().setText("newVALUE\\\n"
+            + "someGoodValue\n"
+            + "[\n"
+            + "(\n"
+            + ".*dbo\n");
+        page.getIncludeSchema().setSelected(false);
+        page.getSkipNullValues().setSelected(true);
+        try {
+            page.apply();
+            fail("Should throw an exception");
+        } catch (ConfigurationException e) {
+            assertEquals("Invalid regular expressions in lines: 1, 3, 4", e.getMessage());
+        }
+
+        // then
+        verify(projectSettings, times(0)).setProperties(anyObject(), anyBoolean(), anyBoolean(), anyBoolean(), anyString());
     }
 
     private ProjectSettingsPage preparePageWithMocks() {
