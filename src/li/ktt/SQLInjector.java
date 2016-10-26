75513a2feca4e77e9764d0ad82038a97c0089355
Pawel 'kTT' Salata <rockplayer.pl@gmail.com>
2016-10-26 20:51:39 +0200

    Adds language injector for dataset files. Adds convert without text selection.

---
+++ src/li/ktt/SQLInjector.java
@@ -0,0 +1,27 @@
+package li.ktt;
+
+import com.intellij.lang.Language;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.InjectedLanguagePlaces;
+import com.intellij.psi.LanguageInjector;
+import com.intellij.psi.PsiLanguageInjectionHost;
+import org.jetbrains.annotations.NotNull;
+
+public class SQLInjector implements LanguageInjector {
+    @Override
+    public void getLanguagesToInject(@NotNull final PsiLanguageInjectionHost host,
+                                     @NotNull final InjectedLanguagePlaces places) {
+        final boolean isSelectQuery = host.getText().trim().toUpperCase().startsWith("SELECT");
+        final boolean isDataSetFile = host.getContainingFile().getText().startsWith("<dataset>");
+        if (isDataSetFile && isSelectQuery) {
+            final Language language = Language.findLanguageByID("SQL");
+            if (language != null) {
+                try {
+                    places.addPlace(language, TextRange.from(0, host.getTextLength()), null, null);
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
